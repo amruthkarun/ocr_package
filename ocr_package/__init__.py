8b1d5198485d467302fbd04598a561fa507e1633
Qunfei Wu <qunfei.wu@allianz.com>
2022-03-01 17:26:56 +0100

    Bump version: 0.1.1 → 0.1.2

--- ocr_package/__init__.py
+++ ocr_package/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """Qunfei Wu"""
 __email__ = 'qunfei.wu@outlook.com'
-__version__ = '__version__ = '__version__ = '__version__ = '__version__ = '__version__ = '__version__ = '0.1.1'''''''
+__version__ = '__version__ = '__version__ = '__version__ = '__version__ = '__version__ = '__version__ = '0.1.2'''''''
