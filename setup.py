1a68001361664190b953a65d17c1899a113d616f
Qunfei Wu <qunfei.wu@allianz.com>
2022-03-01 17:39:35 +0100

    Bump version: 0.1.3 → 0.1.4

--- setup.py
+++ setup.py
@@ -44,6 +44,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/qunfei-wu/ocr_package',
-    version='0.1.3',
+    version='0.1.4',
     zip_safe=False,
 )
